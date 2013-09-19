f81373abc16f668f3d0f7105753fa35605c876c5
Mark Taylor <marktaylor1@digital.cabinet-office.gov.uk>
2013-09-19 14:32:20 +0100

    [#532] Mark - Modified cert-chain validation to work with a trust store:
    - Added the IDAP intermediary CA cert to the truststore under deploy/keys and included a copy of the truststore in the test resources of the shared-rest module.

--- CertificateChainValidator.java
+++ CertificateChainValidator.java
@@ -1,39 +1,33 @@
 package uk.gov.ida.shared.rest.config.verification;
 
 import com.google.common.collect.ImmutableList;
+import com.google.inject.Inject;
 import uk.gov.ida.shared.rest.truststore.DependentServiceSSLTrustStore;
 
-import java.security.InvalidAlgorithmParameterException;
-import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertPath;
 import java.security.cert.CertPathValidator;
-import java.security.cert.CertPathValidatorException;
-import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.PKIXParameters;
-import java.security.cert.TrustAnchor;
 import java.security.cert.X509Certificate;
-import java.util.Collections;
-import java.util.List;
 
 
 public class CertificateChainValidator {
     public final String PKIX_ALGORITHM = "PKIX";
     public final String X509_CERTIFICATE_TYPE = "X.509";
+    private DependentServiceSSLTrustStore trustStore;
 
-    public void validate(
-            X509Certificate certificate,
-            DependentServiceSSLTrustStore trustStore) throws Exception {
+    @Inject
+    public CertificateChainValidator() {
+    }
+
+    public CertificateChainValidator(DependentServiceSSLTrustStore trustStore) {
+        this.trustStore = trustStore;
+    }
+
+    public void validate(X509Certificate certificate) throws Exception {
 
         PKIXParameters certPathParameters = new PKIXParameters(trustStore.getKeyStore().get());
         final ImmutableList<X509Certificate> certificateChain = ImmutableList.of(certificate);
-
-        validate(certPathParameters, certificateChain);
-    }
-
-    private void validate(
-            PKIXParameters certPathParameters,
-            List<X509Certificate> certificateChain) throws CertificateException, NoSuchAlgorithmException, CertPathValidatorException, InvalidAlgorithmParameterException {
 
         CertificateFactory certificateFactory = CertificateFactory.getInstance(X509_CERTIFICATE_TYPE);
         CertPath certPath = certificateFactory.generateCertPath(certificateChain);
