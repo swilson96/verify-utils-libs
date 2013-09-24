c27ccc496e6a0f9ff3a99550af79b34ca5deec18
Mark Taylor <marktaylor1@digital.cabinet-office.gov.uk>
2013-09-24 11:23:42 +0100

    [#532] Mark - Validate certificates returned by config service:
    - Any callers that retrieve signing and encryption certs from the config service now validate that the returned cert can be chained with those in the IDA trust store.
    - Renamed DependentServiceSSLTrustStoreProvider* classes to IdaTrustStore* since the trust store stuff is not just for SSL now.

--- CertificateChainValidator.java
+++ CertificateChainValidator.java
@@ -1,41 +1,64 @@
 package uk.gov.ida.shared.rest.config.verification;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.base.Throwables;
 import com.google.inject.Inject;
-import uk.gov.ida.shared.rest.truststore.DependentServiceSSLTrustStore;
+import uk.gov.ida.shared.rest.truststore.IdaTrustStore;
 
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertPath;
 import java.security.cert.CertPathValidator;
+import java.security.cert.CertPathValidatorException;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.PKIXParameters;
 import java.security.cert.X509Certificate;
+
+import static com.google.common.collect.ImmutableList.of;
 
 
 public class CertificateChainValidator {
     public final String PKIX_ALGORITHM = "PKIX";
     public final String X509_CERTIFICATE_TYPE = "X.509";
-    private DependentServiceSSLTrustStore trustStore;
+    public PKIXParameters certPathParameters;
+    public CertificateFactory certificateFactory;
+    public CertPathValidator certPathValidator;
 
     @Inject
-    public CertificateChainValidator() {
-    }
-
-    public CertificateChainValidator(DependentServiceSSLTrustStore trustStore) {
-        this.trustStore = trustStore;
-    }
-
-    public void validate(X509Certificate certificate) throws Exception {
-
-        PKIXParameters certPathParameters = new PKIXParameters(trustStore.getKeyStore().get());
-        final ImmutableList<X509Certificate> certificateChain = ImmutableList.of(certificate);
-
-        CertificateFactory certificateFactory = CertificateFactory.getInstance(X509_CERTIFICATE_TYPE);
-        CertPath certPath = certificateFactory.generateCertPath(certificateChain);
-
+    public CertificateChainValidator(IdaTrustStore trustStore) {
+        try {
+            certPathParameters = new PKIXParameters(trustStore.getKeyStore().get());
+        } catch (KeyStoreException | InvalidAlgorithmParameterException e) {
+            throw Throwables.propagate(e);
+        }
         certPathParameters.setRevocationEnabled(false);
 
-        CertPathValidator certPathValidator = CertPathValidator.getInstance(PKIX_ALGORITHM);
+        try {
+            certificateFactory = CertificateFactory.getInstance(X509_CERTIFICATE_TYPE);
+        } catch (CertificateException e) {
+            throw Throwables.propagate(e);
+        }
 
-        certPathValidator.validate(certPath, certPathParameters);
+        try {
+            certPathValidator = CertPathValidator.getInstance(PKIX_ALGORITHM);
+        } catch (NoSuchAlgorithmException e) {
+            throw Throwables.propagate(e);
+        }
+    }
+
+    public void validate(X509Certificate certificate) {
+        CertPath certificatePath;
+        try {
+            certificatePath = certificateFactory.generateCertPath(of(certificate));
+        } catch (CertificateException e) {
+            throw Throwables.propagate(e);
+        }
+
+        try {
+            certPathValidator.validate(certificatePath, certPathParameters);
+        } catch (CertPathValidatorException | InvalidAlgorithmParameterException e) {
+            throw Throwables.propagate(e);
+        }
     }
 }
