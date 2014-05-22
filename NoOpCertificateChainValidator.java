844c1e05101710dc7347acff976fe1a1da159eeb
Mark <zoodor+github@gmail.com>
2014-05-22 17:12:04 +0100

    1152: Rename config package to configuration
    Pair: Peter/Mark

---
+++ NoOpCertificateChainValidator.java
@@ -0,0 +1,21 @@
+package uk.gov.ida.matchingserviceadapter.configuration.verification;
+
+import com.google.inject.Inject;
+import uk.gov.ida.matchingserviceadapter.rest.common.transformers.CertificateDtoToX509CertificateTransformer;
+import uk.gov.ida.matchingserviceadapter.rest.truststore.IdaTrustStore;
+
+import java.security.cert.X509Certificate;
+
+public class NoOpCertificateChainValidator extends CertificateChainValidator {
+
+    @Inject
+    public NoOpCertificateChainValidator(
+            IdaTrustStore trustStore,
+            CertificateDtoToX509CertificateTransformer certificateTransformer) {
+
+        super(trustStore, certificateTransformer);
+    }
+
+    public void validate(X509Certificate certificate) {
+    }
+}
