7af8990b2ff303317d072b5d288afc658b26d2e1
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2014-06-20 14:03:26 +0100

    870: Support mutual auth on stub-registration
    Pair: Jenny/Chris

--- ConnectionInitiatorConfiguration.java
+++ ConnectionInitiatorConfiguration.java
@@ -5,6 +5,7 @@ import io.dropwizard.client.JerseyClientConfiguration;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.io.File;
 
 public abstract class ConnectionInitiatorConfiguration extends BaseApplicationConfiguration {
 
@@ -17,11 +18,36 @@ public abstract class ConnectionInitiatorConfiguration extends BaseApplicationCo
     @JsonProperty
     protected boolean acceptSelfSignedCerts = false;
 
+    public MutualAuthConfiguration getMutualAuth() {
+        return mutualAuth;
+    }
+
+    @JsonProperty
+    protected MutualAuthConfiguration mutualAuth = null;
+
     public boolean acceptsSelfSignedCerts() {
         return acceptSelfSignedCerts;
     }
 
     public JerseyClientConfiguration getClientConnectionConfig() {
         return httpClient;
+    }
+
+    public static class MutualAuthConfiguration {
+        @JsonProperty
+        @NotNull
+        private File keyStoreFile;
+
+        @JsonProperty
+        @NotNull
+        private String keyStorePassword;
+
+        public String getKeyStorePassword() {
+            return keyStorePassword;
+        }
+
+        public File getKeyStoreFile() {
+            return keyStoreFile;
+        }
     }
 }
