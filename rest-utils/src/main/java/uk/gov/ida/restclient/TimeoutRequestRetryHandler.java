73cbaeb88cfec6dd8b0343a4a4d06358785a6766
kputt <kputt@thoughtworks.com>
2014-07-08 11:48:03 +0100

    bau: added logging and testing to TimeoutRequestRetryHandler

--- rest-utils/src/main/java/uk/gov/ida/restclient/TimeoutRequestRetryHandler.java
+++ rest-utils/src/main/java/uk/gov/ida/restclient/TimeoutRequestRetryHandler.java
@@ -1,12 +1,20 @@
 package uk.gov.ida.restclient;
 
+import org.apache.http.HttpRequest;
+import org.apache.http.RequestLine;
 import org.apache.http.client.HttpRequestRetryHandler;
 import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.protocol.HttpContext;
+import org.apache.http.protocol.HttpCoreContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
+import static java.text.MessageFormat.format;
+
 public class TimeoutRequestRetryHandler implements HttpRequestRetryHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(TimeoutRequestRetryHandler.class);
     private final int numRetries;
 
     public TimeoutRequestRetryHandler(int numRetries) {
@@ -15,7 +23,20 @@ public class TimeoutRequestRetryHandler implements HttpRequestRetryHandler {
 
     @Override
     public boolean retryRequest(IOException e, int executionCount, HttpContext httpContext) {
-        if( e instanceof ConnectTimeoutException && executionCount < numRetries) {
+        if (e instanceof ConnectTimeoutException && executionCount <= numRetries) {
+            String uri = null;
+            String httpMethod = null;
+            try {
+                final HttpCoreContext coreContext = HttpCoreContext.adapt(httpContext);
+                final HttpRequest request = coreContext.getRequest();
+                final RequestLine requestLine = request.getRequestLine();
+                uri = requestLine.getUri();
+                httpMethod = requestLine.getMethod();
+            }
+            finally {
+                LOG.info(format("Retrying {0} of {1}, to {2} / {3}", executionCount, numRetries, httpMethod, uri));
+
+            }
             return true;
         }
         return false;
