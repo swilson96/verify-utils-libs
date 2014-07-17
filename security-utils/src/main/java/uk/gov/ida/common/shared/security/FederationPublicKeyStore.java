4d5d9c02443c6d8c221c68eb1c4621b384e5a125
Adigo Atabo <aadigo@euaadigo.local>
2014-07-17 16:46:30 +0100

    1298: Added trustStore parameter to federation public keystore interface
    Pair: Sudarshan / Adigo

--- security-utils/src/main/java/uk/gov/ida/common/shared/security/FederationPublicKeyStore.java
+++ security-utils/src/main/java/uk/gov/ida/common/shared/security/FederationPublicKeyStore.java
@@ -1,9 +1,11 @@
 package uk.gov.ida.common.shared.security;
 
+import uk.gov.ida.truststore.IdaTrustStore;
+
 import java.security.PublicKey;
 import java.util.List;
 
 public interface FederationPublicKeyStore {
-    List<PublicKey> getVerifyingKeysForEntity(String entityId);
-    PublicKey getEncryptionKeyForEntity(String entityId);
+    List<PublicKey> getVerifyingKeysForEntity(String entityId, IdaTrustStore trustStore);
+    PublicKey getEncryptionKeyForEntity(String entityId, IdaTrustStore trustStore);
 }
