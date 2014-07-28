2a98a61440d113ccc5fbca1fe47f3657ca3b4ddd
Jenny Ramsay <jramsay@thoughtworks.com>
2014-07-28 14:19:28 +0100

    1315: Change client to use TLSv1.2 rather than SSL

--- rest-utils/src/main/java/uk/gov/ida/restclient/SecureSSLJerseyClientBuilder.java
+++ rest-utils/src/main/java/uk/gov/ida/restclient/SecureSSLJerseyClientBuilder.java
@@ -56,7 +56,7 @@ public class SecureSSLJerseyClientBuilder {
     private SecureSSLClientConfiguration getSecureSSLClientConfiguration() {
         SSLContext sslContext;
         try {
-            sslContext = SSLContext.getInstance("SSL");
+            sslContext = SSLContext.getInstance("TLSv1.2");
         } catch (Exception e) {
             LOG.error("Error creating SSL context.", e);
             throw Throwables.propagate(e);
