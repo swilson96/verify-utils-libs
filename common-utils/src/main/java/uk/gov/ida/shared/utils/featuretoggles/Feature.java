79ee45f7cafbdb14190e25656799c9fdd8dc1459
kputt <kputt@thoughtworks.com>
2014-08-12 16:36:42 +0100

    1390: Removed unused create date
    Pair: David K

--- common-utils/src/main/java/uk/gov/ida/shared/utils/featuretoggles/Feature.java
+++ common-utils/src/main/java/uk/gov/ida/shared/utils/featuretoggles/Feature.java
@@ -1,9 +1,6 @@
 package uk.gov.ida.shared.utils.featuretoggles;
 
-import org.joda.time.DateTime;
-
 public interface Feature {
     public void setActive(boolean active);
     public boolean isActive();
-    public DateTime getCreateDate();
 }
