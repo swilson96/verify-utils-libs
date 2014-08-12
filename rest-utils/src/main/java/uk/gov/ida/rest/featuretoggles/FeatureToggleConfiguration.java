0ab1db4cb1e108f48953fa9eec3a01c0ac8bcacb
kputt <kputt@thoughtworks.com>
2014-08-12 13:08:19 +0100

    1390: created feature toggle bundle

---
+++ rest-utils/src/main/java/uk/gov/ida/rest/featuretoggles/FeatureToggleConfiguration.java
@@ -0,0 +1,8 @@
+package uk.gov.ida.rest.featuretoggles;
+
+
+import uk.gov.ida.shared.utils.featuretoggles.FeatureConfiguration;
+
+public interface FeatureToggleConfiguration {
+    public FeatureConfiguration getFeatureConfiguration();
+}
