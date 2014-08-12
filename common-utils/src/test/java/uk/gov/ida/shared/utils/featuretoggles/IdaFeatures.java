01a4e87bfdd6a415283fad72216b7aa3a072ac9e
kputt <kputt@thoughtworks.com>
2014-08-12 16:42:29 +0100

    1390: Fixed IdaFeatures to not implement getCreatedDate in test
    and removed FeatureTest that no longer tested anything
    Pair: David K

--- common-utils/src/test/java/uk/gov/ida/shared/utils/featuretoggles/IdaFeatures.java
+++ common-utils/src/test/java/uk/gov/ida/shared/utils/featuretoggles/IdaFeatures.java
@@ -1,27 +1,14 @@
 package uk.gov.ida.shared.utils.featuretoggles;
 
-import org.joda.time.DateTime;
-
 public enum IdaFeatures implements Feature {
 
-    UIRework(DateTime.parse("2014-08-14")),
-    EncodeAssertions(DateTime.parse("2014-08-14"));
-
-    private final DateTime createdDate;
-
-    IdaFeatures(DateTime createdDate) {
-        this.createdDate = createdDate;
-    }
+    UIRework,
+    EncodeAssertions;
 
     private boolean active;
 
     public boolean isActive() {
         return active;
-    }
-
-    @Override
-    public DateTime getCreateDate() {
-        return createdDate;
     }
 
     public void setActive(boolean active) {
