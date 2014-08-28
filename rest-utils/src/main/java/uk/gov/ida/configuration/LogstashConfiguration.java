2802f96dda7042286c0b063310aa15194b983b7f
adigo <adigo.atabo@digital.cabinet-office.gov.uk>
2014-08-28 14:19:04 +0100

    1466: Remove LogstashConfiguration class
    LogstashConfiguration class not used anywhere
    
    Pair: Archanaa/Adigo

--- rest-utils/src/main/java/uk/gov/ida/configuration/LogstashConfiguration.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.ida.configuration;
-
-import ch.qos.logback.classic.Level;
-
-public interface LogstashConfiguration {
-    Boolean shouldUseLogstashFormat();
-
-    Level getLogLevel();
-}
