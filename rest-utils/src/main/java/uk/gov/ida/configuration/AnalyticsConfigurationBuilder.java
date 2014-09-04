60856b5e61269ab5e070611b2246d0faf9df0c7e
Oswald <Oswald.quek@digital.cabinet-office.gov.uk>
2014-09-04 11:24:51 +0100

    bau: allow option to diable analytics
    Driven by the want to disable analytics for frontend integration tests

--- rest-utils/src/main/java/uk/gov/ida/configuration/AnalyticsConfigurationBuilder.java
+++ rest-utils/src/main/java/uk/gov/ida/configuration/AnalyticsConfigurationBuilder.java
@@ -2,13 +2,20 @@ package uk.gov.ida.configuration;
 
 public class AnalyticsConfigurationBuilder {
 
+    private boolean enabled = true;
+
     public static AnalyticsConfigurationBuilder anAnalyticsConfiguration() {
         return new AnalyticsConfigurationBuilder();
     }
 
     public AnalyticsConfiguration build() {
-        return new TestAnalyticsConfiguration(true, 9595, "http://piwik-digds.rhcloud.com/analytics", "http://analytics-1/analytics");
+        return new TestAnalyticsConfiguration(enabled, 9595, "http://piwik-digds.rhcloud.com/analytics", "http://analytics-1/analytics");
 
+    }
+
+    public AnalyticsConfigurationBuilder setEnabled(boolean enabled) {
+        this.enabled = enabled;
+        return this;
     }
 
     private static class TestAnalyticsConfiguration extends AnalyticsConfiguration {
