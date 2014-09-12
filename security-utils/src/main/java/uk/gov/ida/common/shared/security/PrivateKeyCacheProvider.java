16438f82a302cc9bb143fe472c21c9bce708310e
David King <david.king@digital.cabinet-office.gov.uk>
2014-09-12 16:48:59 +0100

    1541: Revert PrivateKeyCacheProvider changes
    No need to commit any changes here as we've added the PrivateKeyCache.
    
    Pair: DavidK

--- security-utils/src/main/java/uk/gov/ida/common/shared/security/PrivateKeyCacheProvider.java
+++ security-utils/src/main/java/uk/gov/ida/common/shared/security/PrivateKeyCacheProvider.java
@@ -16,13 +16,9 @@ import java.security.PrivateKey;
 import java.util.concurrent.ExecutionException;
 
 public class PrivateKeyCacheProvider implements Provider<LoadingCache<String, PrivateKey>> {
-    private static final String ENCRYPTION_KEY_ENVIRONMENT_VARIABLE = "ENCRYPTION_KEY";
-    private static final String SIGNING_KEY_ENVIRONMENT_VARIABLE = "SIGNING_KEY";
     private KeyConfiguration signingKeyConfiguration;
     private KeyConfiguration encryptionKeyConfiguration;
     private PrivateKeyFactory privateKeyFactory;
-    private LoadingCache<String, PrivateKey> initializedValue;
-
     @Inject
     public PrivateKeyCacheProvider(
             @PrivateSigningKeyConfiguration KeyConfiguration signingKeyConfiguration,
@@ -32,15 +28,10 @@ public class PrivateKeyCacheProvider implements Provider<LoadingCache<String, Pr
         this.signingKeyConfiguration = signingKeyConfiguration;
         this.encryptionKeyConfiguration = encryptionKeyConfiguration;
         this.privateKeyFactory = privateKeyFactory;
-        initializedValue = init();
     }
 
     @Override
     public LoadingCache<String, PrivateKey> get() {
-        return initializedValue;
-    }
-
-    private LoadingCache<String, PrivateKey> init() {
         LoadingCache<String, PrivateKey> keyCache = CacheBuilder.<String, PrivateKey>newBuilder()
                 .build(new CacheLoader<String, PrivateKey>() {
                     @Override
@@ -50,23 +41,13 @@ public class PrivateKeyCacheProvider implements Provider<LoadingCache<String, Pr
                 });
 
         try {
-            keyCache.get(getSigningKeyPath());
-            keyCache.get(getEncryptionKeyPath());
+            keyCache.get(signingKeyConfiguration.getKeyUri());
+            keyCache.get(encryptionKeyConfiguration.getKeyUri());
         } catch (ExecutionException e) {
            // Swallowing this exception because this is simply warming up the cache
         }
 
         return keyCache;
-    }
-
-    private String getSigningKeyPath() {
-        String signingKeyPathFromEnv = System.getenv(SIGNING_KEY_ENVIRONMENT_VARIABLE);
-        return signingKeyPathFromEnv == null ? signingKeyConfiguration.getKeyUri() : signingKeyPathFromEnv;
-    }
-
-    private String getEncryptionKeyPath() {
-        String encryptionKeyPathFromEnv = System.getenv(ENCRYPTION_KEY_ENVIRONMENT_VARIABLE);
-        return encryptionKeyPathFromEnv == null ? encryptionKeyConfiguration.getKeyUri() : encryptionKeyPathFromEnv;
     }
 
     private PrivateKey getPrivateKey(String keyUri) throws IOException {
