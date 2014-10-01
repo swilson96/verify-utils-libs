4fdfd6556bd58d908a12751cd15c6e96357e8da9
Guy Koth <gkoth@thoughtworks.com>
2014-10-01 14:02:35 +0100

    1648: remove option to read private keys from disk
    Private keys will always now be read from FileDescriptors, whatever args are given to PrivateKeyCache

--- security-utils/src/main/java/uk/gov/ida/common/shared/security/PrivateKeyCache.java
+++ security-utils/src/main/java/uk/gov/ida/common/shared/security/PrivateKeyCache.java
@@ -1,23 +1,15 @@
 package uk.gov.ida.common.shared.security;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.io.Files;
 import uk.gov.ida.common.shared.configuration.KeyConfiguration;
-import uk.gov.ida.common.shared.security.exceptions.KeyLoadingException;
 
-import java.io.File;
-import java.io.IOException;
 import java.security.PrivateKey;
+import java.util.List;
 
 public class PrivateKeyCache implements PrivateKeyStore {
-    private KeyConfiguration signingKeyConfiguration;
-    private KeyConfiguration primaryEncryptionKeyConfiguration;
-    private KeyConfiguration secondaryEncryptionKeyConfiguration;
-    private PrivateKeyFactory privateKeyFactory;
     private NumberedPipeReader numberedPipeReader;
-    private boolean secureKeyManagementEnabled;
-    private PrivateKey signingKey;
 
+    private PrivateKey signingKey;
     private PrivateKey primaryEncryptionKey;
     private PrivateKey secondaryEncryptionKey;
 
@@ -25,6 +17,12 @@ public class PrivateKeyCache implements PrivateKeyStore {
     private static final int SECONDARY_ENCRYPTION_PRIVATE_KEY_FD = 5;
     private static final int SIGNING_PRIVATE_KEY_FD = 6;
 
+    public PrivateKeyCache(NumberedPipeReader numberedPipeReader) {
+        this.numberedPipeReader = numberedPipeReader;
+        getKeys();
+    }
+
+    @Deprecated
     public PrivateKeyCache(
             KeyConfiguration signingKeyConfiguration,
             KeyConfiguration primaryEncryptionKeyConfiguration,
@@ -32,14 +30,7 @@ public class PrivateKeyCache implements PrivateKeyStore {
             PrivateKeyFactory privateKeyFactory,
             NumberedPipeReader numberedPipeReader,
             boolean secureKeyManagementEnabled) {
-
-        this.secureKeyManagementEnabled = secureKeyManagementEnabled;
-        this.signingKeyConfiguration = signingKeyConfiguration;
-        this.primaryEncryptionKeyConfiguration = primaryEncryptionKeyConfiguration;
-        this.secondaryEncryptionKeyConfiguration = secondaryEncryptionKeyConfiguration;
-        this.privateKeyFactory = privateKeyFactory;
-        this.numberedPipeReader = numberedPipeReader;
-        getKeys();
+        this(numberedPipeReader);
     }
 
     @Override
@@ -48,43 +39,13 @@ public class PrivateKeyCache implements PrivateKeyStore {
     }
 
     @Override
-    public java.util.List<PrivateKey> getEncryptionPrivateKeys() {
+    public List<PrivateKey> getEncryptionPrivateKeys() {
         return ImmutableList.of(primaryEncryptionKey, secondaryEncryptionKey);
     }
 
     private void getKeys() {
-        if(this.secureKeyManagementEnabled){
-            this.signingKey = this.numberedPipeReader.readKey(SIGNING_PRIVATE_KEY_FD);
-            this.primaryEncryptionKey = this.numberedPipeReader.readKey(PRIMARY_ENCRYPTION_PRIVATE_KEY_FD);
-            this.secondaryEncryptionKey = this.numberedPipeReader.readKey(SECONDARY_ENCRYPTION_PRIVATE_KEY_FD);
-        }
-        else {
-            this.signingKey = getSigningKeyFromConfigUri();
-            this.primaryEncryptionKey = getPrimaryEncryptionKeyFromConfigUri();
-            this.secondaryEncryptionKey = getSecondaryEncryptionKeyFromUri();
-        }
-    }
-
-    private PrivateKey getSigningKeyFromConfigUri() {
-            String signingKeyPath = signingKeyConfiguration.getKeyUri();
-            return getPrivateKey(signingKeyPath);
-    }
-
-    private PrivateKey getPrimaryEncryptionKeyFromConfigUri() {
-        String encryptionKeyPath = primaryEncryptionKeyConfiguration.getKeyUri();
-        return getPrivateKey(encryptionKeyPath);
-    }
-
-    private PrivateKey getSecondaryEncryptionKeyFromUri() {
-            String encryptionKeyPath = secondaryEncryptionKeyConfiguration.getKeyUri();
-            return getPrivateKey(encryptionKeyPath);
-    }
-
-    private PrivateKey getPrivateKey(String keyUri) {
-        try {
-            return privateKeyFactory.createPrivateKey(Files.toByteArray(new File(keyUri)));
-        } catch (IOException e) {
-            throw new KeyLoadingException(keyUri, e);
-        }
+        this.signingKey = this.numberedPipeReader.readKey(SIGNING_PRIVATE_KEY_FD);
+        this.primaryEncryptionKey = this.numberedPipeReader.readKey(PRIMARY_ENCRYPTION_PRIVATE_KEY_FD);
+        this.secondaryEncryptionKey = this.numberedPipeReader.readKey(SECONDARY_ENCRYPTION_PRIVATE_KEY_FD);
     }
 }
--- security-utils/src/test/java/uk/gov/ida/common/shared/security/PrivateKeyCacheTest.java
+++ security-utils/src/test/java/uk/gov/ida/common/shared/security/PrivateKeyCacheTest.java
@@ -1,36 +1,21 @@
 package uk.gov.ida.common.shared.security;
 
 import com.google.common.io.Files;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import uk.gov.ida.common.shared.configuration.KeyConfiguration;
 
-import java.io.File;
 import java.security.PrivateKey;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
-import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Files.class, PrivateKeyCache.class})
+@PrepareForTest(Files.class)
 public class PrivateKeyCacheTest {
-    @Mock
-    private KeyConfiguration signingKeyConfiguration;
-    @Mock
-    private KeyConfiguration primaryEncryptionKeyConfiguration;
-    @Mock
-    private KeyConfiguration secondaryEncryptionKeyConfiguration;
-    @Mock
-    private PrivateKeyFactory privateKeyFactory;
     @Mock
     private PrivateKey signingPrivateKey;
     @Mock
@@ -40,88 +25,13 @@ public class PrivateKeyCacheTest {
     @Mock
     private NumberedPipeReader numberedPipeReader;
 
-    private final String nonExistentFilePath = "/boo/encryption-key-uri";
-    private final String tempSigningKeyUri = "/tmp/signing-key-uri";
-    private final String signingKeyUri = "/signing-key-uri";
-    private final String primaryEncryptionKeyUri = "/primary-encryption-key-uri";
-    private final String secondaryEncryptionKeyUri = "/secondary-encryption-key-uri";
-    private final byte[] signingKey = new byte[] {1,2,3};
-    private final byte[] primaryEncryptionKey = new byte[] {4,5,6};
-    private final byte[] secondaryEncryptionKey = new byte[] {7,8,9};
-    private PrivateKeyCache privateKeyCache;
-
-    @Before
-    public void setUp() throws Exception {
-        PowerMockito.mockStatic(System.class);
-        when(System.getenv("ENCRYPTION_KEY")).thenReturn(null);
-        when(System.getenv("SIGNING_KEY")).thenReturn(null);
-
-        when(signingKeyConfiguration.getKeyUri()).thenReturn(signingKeyUri);
-        when(primaryEncryptionKeyConfiguration.getKeyUri()).thenReturn(primaryEncryptionKeyUri);
-        when(secondaryEncryptionKeyConfiguration.getKeyUri()).thenReturn(secondaryEncryptionKeyUri);
-        when(privateKeyFactory.createPrivateKey(eq(signingKey))).thenReturn(signingPrivateKey);
-        when(privateKeyFactory.createPrivateKey(eq(primaryEncryptionKey))).thenReturn(primaryEncryptionPrivateKey);
-        when(privateKeyFactory.createPrivateKey(eq(secondaryEncryptionKey))).thenReturn(secondaryEncryptionPrivateKey);
-    }
-
     @Test
-    public void init_shouldLoadSigningKeyFromDisk() throws Exception {
-        PowerMockito.mockStatic(Files.class);
-        when(Files.toByteArray(new File(signingKeyUri))).thenReturn(signingKey);
-        when(Files.toByteArray(new File(primaryEncryptionKeyUri))).thenReturn(primaryEncryptionKey);
-
-
-        privateKeyCache = new PrivateKeyCache(
-                signingKeyConfiguration,
-                primaryEncryptionKeyConfiguration,
-                secondaryEncryptionKeyConfiguration,
-                privateKeyFactory,
-                numberedPipeReader,
-                false
-        );
-
-        PrivateKey privateKey = privateKeyCache.getSigningPrivateKey();
-        assertThat(privateKey).isEqualTo(signingPrivateKey);
-        verifyStatic(times(1));
-    }
-
-    @Test
-    public void init_shouldLoadEncryptionKeyFromDisk() throws Exception {
-        PowerMockito.mockStatic(Files.class);
-        when(Files.toByteArray(new File(signingKeyUri))).thenReturn(signingKey);
-        when(Files.toByteArray(new File(primaryEncryptionKeyUri))).thenReturn(primaryEncryptionKey);
-        when(Files.toByteArray(new File(secondaryEncryptionKeyUri))).thenReturn(secondaryEncryptionKey);
-
-        privateKeyCache = new PrivateKeyCache(
-                signingKeyConfiguration,
-                primaryEncryptionKeyConfiguration,
-                secondaryEncryptionKeyConfiguration,
-                privateKeyFactory,
-                numberedPipeReader,
-                false
-        );
-
-        List<PrivateKey> encryptionPrivateKeys = privateKeyCache.getEncryptionPrivateKeys();
-        assertThat(encryptionPrivateKeys).hasSize(2);
-        assertThat(encryptionPrivateKeys.get(0)).isEqualTo(primaryEncryptionPrivateKey);
-        assertThat(encryptionPrivateKeys.get(1)).isEqualTo(secondaryEncryptionPrivateKey);
-        verifyStatic(times(1));
-    }
-
-    @Test
-    public void init_shouldLoadKeysFromFileDescriptorWhenLoadSecureKeysIsTrue() throws Exception {
+    public void init_shouldLoadKeysFromFileDescriptor() throws Exception {
         when(numberedPipeReader.readKey(4)).thenReturn(primaryEncryptionPrivateKey);
         when(numberedPipeReader.readKey(5)).thenReturn(secondaryEncryptionPrivateKey);
         when(numberedPipeReader.readKey(6)).thenReturn(signingPrivateKey);
 
-        privateKeyCache = new PrivateKeyCache(
-                null,
-                null,
-                null,
-                null,
-                numberedPipeReader,
-                true
-        );
+        PrivateKeyCache privateKeyCache = new PrivateKeyCache(numberedPipeReader);
 
         List<PrivateKey> encryptionPrivateKeys = privateKeyCache.getEncryptionPrivateKeys();
         PrivateKey actualSigningPrivateKey = privateKeyCache.getSigningPrivateKey();
