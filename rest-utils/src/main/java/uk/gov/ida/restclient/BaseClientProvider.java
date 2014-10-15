2e8ba016c60b8f7a0ea8a32f9fd56e2b580228df
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2014-10-15 15:39:44 +0100

    1772: remove JerseyClientBuilder classes
    These classes were near-duplicates of each other, apart from the
    SSLClientConfiguration bits which didn't need all of the other fields.
    Remove the middleman and move the private methods to BaseClientProvider.

--- rest-utils/src/main/java/uk/gov/ida/restclient/BaseClientProvider.java
+++ rest-utils/src/main/java/uk/gov/ida/restclient/BaseClientProvider.java
@@ -1,12 +1,24 @@
 package uk.gov.ida.restclient;
 
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.Provider;
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.client.urlconnection.HTTPSProperties;
 import io.dropwizard.client.JerseyClientConfiguration;
 import io.dropwizard.setup.Environment;
 import org.apache.http.client.HttpRequestRetryHandler;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
 import org.apache.http.impl.client.StandardHttpRequestRetryHandler;
 import uk.gov.ida.truststore.IdaTrustStore;
+
+import javax.net.ssl.SSLContext;
+import java.util.Map;
+
+import static uk.gov.ida.restclient.IdaClientBuilder.anIdaJerseyClientWithScheme;
+import static uk.gov.ida.restclient.InsecureSSLSchemeRegistryBuilder.aConfigWithInsecureSSLSchemeRegistry;
+import static uk.gov.ida.restclient.SecureSSLSchemeRegistryBuilder.aConfigWithSecureSSLSchemeRegistry;
 
 public abstract class BaseClientProvider implements Provider<Client> {
 
@@ -26,11 +38,54 @@ public abstract class BaseClientProvider implements Provider<Client> {
             retryHandler = new TimeoutRequestRetryHandler(jerseyClientConfiguration.getRetries());
         }
 
+        SSLClientConfiguration sslClientConfiguration;
         if (doesAcceptSelfSignedCerts) {
-            client = new IgnoreSSLJerseyClientBuilder(environment, jerseyClientConfiguration, enableStaleConnectionCheck, retryHandler).build(clientName);
+            sslClientConfiguration = getInsecureSSLClientConfiguration();
         } else {
-            client = new SecureSSLJerseyClientBuilder(environment, jerseyClientConfiguration, idaTrustStore, enableStaleConnectionCheck, retryHandler).build(clientName);
+            sslClientConfiguration = getSecureSSLClientConfiguration(idaTrustStore);
         }
+        client = anIdaJerseyClientWithScheme(
+                environment,
+                jerseyClientConfiguration,
+                sslClientConfiguration.getSchemeRegistry(),
+                sslClientConfiguration.getConfigurationProperties(),
+                clientName,
+                enableStaleConnectionCheck,
+                retryHandler
+        );
+    }
+
+    private SSLClientConfiguration getInsecureSSLClientConfiguration() {
+        SSLContext sslContext;
+        try {
+            sslContext = SSLContext.getInstance("TLSv1.2");
+        } catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+        SchemeRegistry schemeRegistry = aConfigWithInsecureSSLSchemeRegistry(
+                sslContext
+        );
+
+        HTTPSProperties httpsProperties = new HTTPSProperties(new AllowAllHostnameVerifier(), sslContext);
+        Map<String, Object> configurationProperties = ImmutableMap.<String, Object>of(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES, httpsProperties);
+        return new SSLClientConfiguration(schemeRegistry, configurationProperties);
+    }
+
+    private SSLClientConfiguration getSecureSSLClientConfiguration(IdaTrustStore idaTrustStore) {
+        SSLContext sslContext;
+        try {
+            sslContext = SSLContext.getInstance("TLSv1.2");
+        } catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+        SchemeRegistry schemeRegistry = aConfigWithSecureSSLSchemeRegistry(
+                sslContext,
+                idaTrustStore
+        );
+
+        HTTPSProperties httpsProperties = new HTTPSProperties(new AllowAllHostnameVerifier(), sslContext);
+        Map<String, Object> configurationProperties = ImmutableMap.<String, Object>of(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES, httpsProperties);
+        return new SSLClientConfiguration(schemeRegistry, configurationProperties);
     }
 
     @Override
