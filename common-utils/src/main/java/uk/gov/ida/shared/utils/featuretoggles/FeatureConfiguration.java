bd875da9acd9d301b6b3752f68a371a77408025e
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2014-12-30 09:16:20 +0000

    bau: use ida-utils feature flag config in hub
    Note: fields in class have to be public for the local tests to pass
    
    Pair: WillPa

--- common-utils/src/main/java/uk/gov/ida/shared/utils/featuretoggles/FeatureConfiguration.java
+++ common-utils/src/main/java/uk/gov/ida/shared/utils/featuretoggles/FeatureConfiguration.java
@@ -1,18 +1,46 @@
 package uk.gov.ida.shared.utils.featuretoggles;
 
+import com.google.common.collect.ImmutableList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
+import static java.text.MessageFormat.format;
+
 public class FeatureConfiguration {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FeatureConfiguration.class);
+
     @Valid
-    @NotNull
+    // It is valid for us to have no features i.e. a null/empty list
     public List<FeatureEntry> features;
 
     @Valid
     @NotNull
     public String featureClass;
 
-    public List<FeatureEntry> getFeatures() { return features; }
+    public boolean isFeatureActive(String feature) {
+        for(FeatureEntry entry : features) {
+            if(entry.getFeatureName().equals(feature)) {
+                return entry.isActive();
+            }
+        }
+        LOG.error(format("Attempted to use unknown feature flag: {0}", feature));
+        return false;
+    }
+
+    public int getFeatureCount() {
+        if(features==null) return 0;
+        return features.size();
+    }
+
+    public List<FeatureEntry> getFeatures() {
+        if(features!=null) return features;
+        return ImmutableList.of();
+    }
+
     public String getFeatureClass() { return featureClass; }
 }
--- common-utils/src/test/java/uk/gov/ida/shared/utils/featuretoggles/FeatureConfigurationTest.java
+++ common-utils/src/test/java/uk/gov/ida/shared/utils/featuretoggles/FeatureConfigurationTest.java
@@ -21,10 +21,25 @@ public class FeatureConfigurationTest {
         FeatureConfiguration featureConfiguration = (FeatureConfiguration) yaml.load(is);
 
         assertThat(featureConfiguration.getFeatureClass()).isEqualTo("uk.gov.ida.shared.utils.featuretoggles.IdaFeatures");
-        assertThat(featureConfiguration.getFeatures().size()).isEqualTo(2);
+        assertThat(featureConfiguration.getFeatureCount()).isEqualTo(2);
         assertThat(featureConfiguration.getFeatures().get(0).isActive()).isEqualTo(false);
         assertThat(featureConfiguration.getFeatures().get(0).getFeatureName()).isEqualTo("UIRework");
         assertThat(featureConfiguration.getFeatures().get(1).isActive()).isEqualTo(true);
         assertThat(featureConfiguration.getFeatures().get(1).getFeatureName()).isEqualTo("EncodeAssertions");
+    }
+
+    @Test
+    public void loadFromFile_shouldCreateConfigurationForEmptyFeatureList() throws Exception {
+        Yaml yaml = new Yaml(new Constructor(FeatureConfiguration.class));
+        String fileName = "uk/gov/ida/shared/utils/featuretoggles/no-features.yml";
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream is = classloader.getResourceAsStream(fileName);
+
+        // Parse the YAML file and return the output as a series of Maps and Lists
+        FeatureConfiguration featureConfiguration = (FeatureConfiguration) yaml.load(is);
+
+        assertThat(featureConfiguration.getFeatureClass()).isEqualTo("uk.gov.ida.shared.utils.featuretoggles.IdaFeatures");
+        assertThat(featureConfiguration.getFeatureCount()).isEqualTo(0);
+        assertThat(featureConfiguration.getFeatures()).isNotNull();
     }
 }
