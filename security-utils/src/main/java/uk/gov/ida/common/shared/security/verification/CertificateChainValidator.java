cd6ab58653823aee97e435e5c978eab8adf164ff
Rachel Newstead <racheljnewstead@gmail.com>
2015-02-25 17:11:00 +0000

    Revert "2322: Remove validateOrThrow()"
    This reverts commit f7d521fee8fc11e33a4976393196192d3105566e.

--- security-utils/src/main/java/uk/gov/ida/common/shared/security/verification/CertificateChainValidator.java
+++ security-utils/src/main/java/uk/gov/ida/common/shared/security/verification/CertificateChainValidator.java
@@ -47,6 +47,15 @@ public class CertificateChainValidator {
         }
     }
 
+    public void validateOrThrow(X509Certificate certificate, KeyStore keyStore) {
+        final CertificateValidity certificateValidity = validate(certificate, keyStore);
+        if (!certificateValidity.isValid()) {
+            throw new CertificateChainValidationException(
+                    "Certificate is not valid: " + getDnForCertificate(certificate),
+                    certificateValidity.getException().get());
+        }
+    }
+
     public CertificateValidity validate(X509Certificate certificate, KeyStore trustStore) {
         CertPath certificatePath;
 
