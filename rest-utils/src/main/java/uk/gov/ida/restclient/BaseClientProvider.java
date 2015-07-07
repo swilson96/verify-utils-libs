762b636c5ba27d4cd384458ac822eec6a7355133
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2015-07-07 16:59:41 +0100

    Revert "2159: use the Dropwizard Jersey retry behaviour!"
    This reverts commit b4ce380437844b53f5dab4e9dfef86dfe6a582ed.
    
    Pair: @christopherholmes @vedakanala

--- rest-utils/src/main/java/uk/gov/ida/restclient/BaseClientProvider.java
+++ rest-utils/src/main/java/uk/gov/ida/restclient/BaseClientProvider.java
@@ -5,6 +5,7 @@ import io.dropwizard.client.JerseyClientBuilder;
 import io.dropwizard.client.JerseyClientConfiguration;
 import io.dropwizard.jersey.jackson.JacksonMessageBodyProvider;
 import io.dropwizard.setup.Environment;
+import org.apache.http.client.HttpRequestRetryHandler;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
@@ -14,6 +15,7 @@ import org.apache.http.conn.ssl.SSLContextBuilder;
 import org.apache.http.conn.ssl.SSLContexts;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.conn.ssl.X509HostnameVerifier;
+import org.apache.http.impl.client.StandardHttpRequestRetryHandler;
 import org.apache.http.impl.conn.SystemDefaultRoutePlanner;
 
 import javax.validation.Validation;
@@ -39,12 +41,23 @@ public abstract class BaseClientProvider implements Provider<Client> {
 
         JerseyClientBuilder jerseyClientBuilder = new JerseyClientBuilder(environment)
                 .using(jerseyClientConfiguration)
+                .using(getHttpRequestRetryHandler(jerseyClientConfiguration, enableRetryTimeOutConnections))
                 .using(new SystemDefaultRoutePlanner(ProxySelector.getDefault()))
                 .using(getConnectionSocketFactoryRegistry(doesAcceptSelfSignedCerts, trustStore, hostnameVerifier));
 
         jerseyClientBuilder.withProvider(new JacksonMessageBodyProvider(environment.getObjectMapper(), Validation.buildDefaultValidatorFactory().getValidator()));
 
         client = jerseyClientBuilder.build(clientName);
+    }
+
+    private HttpRequestRetryHandler getHttpRequestRetryHandler(JerseyClientConfiguration jerseyClientConfiguration, boolean enableRetryTimeOutConnections) {
+        HttpRequestRetryHandler retryHandler;
+        if (enableRetryTimeOutConnections) {
+            retryHandler = new TimeoutRequestRetryHandler(jerseyClientConfiguration.getRetries());
+        } else {
+            retryHandler = new StandardHttpRequestRetryHandler(0, false);
+        }
+        return retryHandler;
     }
 
     private Registry<ConnectionSocketFactory> getConnectionSocketFactoryRegistry(boolean doesAcceptSelfSignedCerts, KeyStore trustStore, X509HostnameVerifier hostnameVerifier) {
