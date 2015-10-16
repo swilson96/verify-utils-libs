b5cd47aab0c41fce8cf745a70282294b6cf170d1
Yolina Sotirova <yolina.sotirova@digital.cabinet-office.gov.uk>
2015-10-16 16:01:19 +0100

    2887 : adding list of dependent projects to the pre-commit script
    We will now be reminded what projects depend on ida-utils so we can update them
    once a new version of the lib is published
    
    @yolinasotirova

--- pre-commit.sh
+++ pre-commit.sh
@@ -10,4 +10,26 @@ trap "handle_failure" ERR INT TERM
 
 ./gradlew clean test
 
-log "SUCCESS!"
+log "SUCCESS!\n"
+
+tput setaf 3
+printf "\nOnce you publish a new version of ida-utils "
+tput bold
+printf "PLEASE DON'T FORGET "
+tput sgr0
+tput setaf 3
+printf "to update the following dependent projects:\n"
+
+printf "\n ida-hub"
+printf "\n ida-frontend"
+printf "\n ida-compliance-tool"
+printf "\n ida-sample-rp"
+printf "\n ida-stub-idp"
+printf "\n ida-hub-support"
+printf "\n ida-msa"
+printf "\n doc-checking\n"
+
+tput bold
+printf "\nThank you! :)\n\n"
+tput sgr0
+
