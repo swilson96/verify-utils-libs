41844fdae8dc14c384327a2472cddbf51f849d2b
oswaldquek <oswald.quek@digital.cabinet-office.gov.uk>
2016-12-01 14:46:21 +0000

    bau: Add timeout to JerseyClientConfigurationBuilder
    Adding timeout like it's done here
    (https://github.gds/gds/ida-sample-rp/blob/05c4426eaa4723b1c44258427bd30cb26488e33b/src/main/java/uk/gov/ida/rp/builders/JerseyClientConfigurationBuilder.java#L47)
    and here
    (https://github.gds/gds/ida-compliance-tool/blob/0ce53ab1acb85144740314eb40d89ba2e1b42280/compliance-tool/src/integration-test/java/uk/gov/ida/integrationtest/compliancetool/builders/JerseyClientConfigurationBuilder.java#L25)
    so we can get rid of the duplicate JerseyClientConfigurationBuilders in
    ida-sample-rp and ida-compliance-tool, for example.
    
    @oswaldquek

--- rest-utils/src/main/java/uk/gov/ida/jerseyclient/JerseyClientConfigurationBuilder.java
+++ rest-utils/src/main/java/uk/gov/ida/jerseyclient/JerseyClientConfigurationBuilder.java
@@ -5,6 +5,8 @@ import io.dropwizard.util.Duration;
 
 public class JerseyClientConfigurationBuilder {
 
+    private Duration timeout = Duration.microseconds(500);
+
     public static JerseyClientConfigurationBuilder aJerseyClientConfiguration() {
         return new JerseyClientConfigurationBuilder();
     }
@@ -13,11 +15,16 @@ public class JerseyClientConfigurationBuilder {
         return new TestJerseyClientConfiguration(
                 1,
                 128,
-                Duration.microseconds(500),
+                timeout,
                 Duration.microseconds(500),
                 Duration.hours(1),
                 1024,
                 1024);
+    }
+
+    public JerseyClientConfigurationBuilder withTimeout(Duration timeout) {
+        this.timeout = timeout;
+        return this;
     }
 
     private static class TestJerseyClientConfiguration extends JerseyClientConfiguration {
